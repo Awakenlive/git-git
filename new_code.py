0ff1e915bf28a1aa60f775a96f12fdf027bbcb65
Awakenlive <110718679+Awakenlive@users.noreply.github.com>
2024-09-21 16:02:17 +0300

    new operations in new_code.py

--- new_code.py
+++ new_code.py
@@ -1,4 +1,11 @@
 if 1 > 0:
     print("Докажите это, вдруг 1 < 0")
-a, b = int(input()), int(input())
-print(f"{a} + {b} = {a+b}")
+s = input().split()
+if s[1] == '+':
+    print(int(s[0]) + int(s[2]))
+elif s[1] == '-':
+    print(int(s[0]) - int(s[2]))
+elif s[1] == '*':
+    print(int(s[0]) * int(s[2]))
+elif s[1] == '/':
+    print(int(s[0]) / int(s[2]))
